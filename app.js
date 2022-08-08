dd779f20840d05d75433fd9f1ea7a2610520b368
Berutorion <eagel163013@gmail.com>
2022-08-08 16:02:42 +0800

    feat: create index page

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ require("./config/mongoose");
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", "./views");
+
+//set body-parser
+app.use(express.urlencoded({ extended: true }));
+
 //listen server
 app.listen(port, () => {
   console.log(`Server is working on http://localhost:${port}`);
@@ -15,4 +19,14 @@ app.listen(port, () => {
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/short/create", (req, res) => {
+  const { url } = req.body;
+  console.log(url);
+  res.redirect("/");
+});
+
+app.post("*", (req, res) => {
+  res.send("what!!!?", 404);
 });
